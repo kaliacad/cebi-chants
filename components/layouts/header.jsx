19249f6d3173778dcd19052a5e2d3ab2afe6f043
Josué Makuta <makutajosue@gmail.com>
2023-03-20 21:51:51 +0200

    Style : Header bar

--- components/layouts/header.jsx
+++ components/layouts/header.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="flex pl-10 pr-10 pt-8 justify-between h-[5vw]  ">
-      <div className=" ">
+    <div className="flex pl-10 pr-10 h-[7vw]  justify-between items-center bg-gradient-to-r from-yellow-500 to-white  ">
+      <div className=" text-white ">
         <h1 className=" font-bold text-3xl ">Bienvenu !</h1>
-        <p>Colossiens 6</p>
+        <p className=" font-thin font-Playfair " >Colossiens 6</p>
       </div>
-      <div className=" flex  ">
-        <Image src={"/images/image 3Logo Image.png"} width={100} height={100} />
-        <h1 className=" w-28 font-bold text-xl  ">
+      <div className=" flex  justify-center items-center ">
+        <Image src={"/images/image 3Logo Image.png"} width={50} height={100} />
+        <h1 className=" w-28 font-bold text-xl ">
           Assemblé <span className=" text-red-500 ">Chretienne</span>
         </h1>
       </div>
