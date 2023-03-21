8025b5ced85da5fe74cc4c25f7065f3f0056741a
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-03-21 11:42:54 +0100

    feat : add Feed components

---
+++ components/ChantPopulaire.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChantPopulaire = () => {
+  return (
+    <div>ChantPopulaire</div>
+  )
+}
+
+export default ChantPopulaire
---
+++ components/ChantRecent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChantRecent = () => {
+  return (
+    <div>ChantRecent</div>
+  )
+}
+
+export default ChantRecent
