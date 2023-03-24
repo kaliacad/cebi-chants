af7462bc05a19cc402ad1e81fa1409206eccad9e
AbijaK <abijamk@gmail.com>
2023-03-24 14:50:25 +0100

    refactor: discard changes from layout component

--- components/SongList.jsx
+++ components/SongList.jsx
@@ -50,17 +50,18 @@ const SongList = () => {
     
         <aside className="flex flex-col  justify-between gap-4 sm:w-96">
             <h3 className="text-base pl-3 font-light sm:text-xl sm:pl-7 sm:font-semibold sm:w-96 
-            md:max-xl:text-2xl md:font-semibold md:pl-4 max-xl:font-bold">
+            md:lg:text-2xl md:font-semibold md:pl-4 lg:text-2xl lg:pl-0 lg:font-bold">
                 Tous les chants</h3>
-            <div className="flex flex-col justify-center items-center gap-4 mx-auto w-80 md:max-xl:w-full">
+            <div className="flex flex-col justify-center items-center gap-4 mx-auto w-80 md:lg:w-full">
             {
                 chants?.map((item, id) => (
-                <div key={id} className="bg-gray-200 flex justify-between py-2 px-2 font-light rounded-md w-72 text-xs  sm:text-xs sm:w-80 md:py-3 md:px-3 md:text-sm md:w-11/12">
+                <div key={id} className="bg-gray-200 flex justify-between py-2 px-2 font-light rounded-md w-72 text-xs  
+                sm:text-xs sm:w-80 md:py-3 md:px-3 md:text-sm md:w-11/12 lg:w-96">
                     <div className="flex justify-center items-center gap-2 md:gap-4">
                         <span className="sm:text-sm md:text-base">{item.order}</span>
                         <div className="rounded-full bg-white h-5 w-5 md:h-12 md:w-12
-                        sm:h-10 sm:w-10 md:max-xl:h-12 
-                        md:max-xl:w-12">
+                        sm:h-10 sm:w-10 md:lg:h-12 
+                        md:lg:w-12">
                             {/* <img src="" alt="s" /> */}
                         </div>
                         <div className="flex flex-col">
