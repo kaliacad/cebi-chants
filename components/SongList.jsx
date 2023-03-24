e00530514ed0cfcc39e5c7382d0f440be1a7d5a5
AbijaK <abijamk@gmail.com>
2023-03-24 14:31:38 +0100

    refactor: update font-size

--- components/SongList.jsx
+++ components/SongList.jsx
@@ -49,14 +49,13 @@ const SongList = () => {
     <div className="mx-auto">
     
         <aside className="flex flex-col  justify-between gap-4 sm:w-96">
-            <h3 className="text-xs pl-3 font-normal sm:text-xl sm:pl-7 md:max-xl:text-2xl sm:font-medium md:font-semibold md:pl-4 max-xl:font-bold">Tous les chants</h3>
-            <div className="flex flex-col justify-center items-center gap-4 mx-auto w-80 
-            sm:w-96 md:max-xl:w-full">
+            <h3 className="text-base pl-3 font-light sm:text-xl sm:pl-7 sm:font-semibold sm:w-96 
+            md:max-xl:text-2xl md:font-semibold md:pl-4 max-xl:font-bold">
+                Tous les chants</h3>
+            <div className="flex flex-col justify-center items-center gap-4 mx-auto w-80 md:max-xl:w-full">
             {
                 chants?.map((item, id) => (
-                <div key={id} className="bg-gray-200 flex justify-between py-2 px-2 md:py-3 md:px-3 rounded-md w-72 text-xs sm:text-xs md:text-sm font-light
-                sm:w-80 md:w-11/12">
-                    {/* sm:gap-8 sm:pt-3 sm:px-5 sm:pb-3 */}
+                <div key={id} className="bg-gray-200 flex justify-between py-2 px-2 font-light rounded-md w-72 text-xs  sm:text-xs sm:w-80 md:py-3 md:px-3 md:text-sm md:w-11/12">
                     <div className="flex justify-center items-center gap-2 md:gap-4">
                         <span className="sm:text-sm md:text-base">{item.order}</span>
                         <div className="rounded-full bg-white h-5 w-5 md:h-12 md:w-12
